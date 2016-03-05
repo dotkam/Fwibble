413b16c07f6fe4d7ca2e7b4f59606e85fc1b9f82
Natalie <natalie.s.sharpe@gmail.com>
2016-03-05 15:04:13 -0600

    progress on seperating components into individual files

---
+++ client/components/GameView.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var StoryTitle = require('./StoryTitle.js');
+var StoryBox = require('./StoryBox.js');
+var StoryInput = require('./StoryInput.js');
+
+var GameView = React.createClass({
+	getInitialState: function () {
+		return {
+
+		};
+	},
+	render: function () {
+		return (
+			<div className="GameView">
+				<h1>Fwibble!</h1>
+				<StoryTitle />
+				<StoryBox />
+				<StoryInput />
+			</div>
+		);
+	}
+});
+
+
+React.render(
+	<GameView/>,
+	document.getElementById('app')
+);
+
+// module.exports = GlobalTaskArea;
