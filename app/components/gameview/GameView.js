0a8973a87a4b8d6bb5a33bcae7cb441be5b734a5
Natalie <natalie.s.sharpe@gmail.com>
2016-03-08 12:20:23 -0600

    handling user input with react - progress

--- app/components/gameview/GameView.js
+++ app/components/gameview/GameView.js
@@ -6,16 +6,22 @@ var StoryInput = require('./StoryInput.js');
 
 module.exports = React.createClass({
 
-	render: function() {
-		return (
-			<div>
-				<h1>Fwibble!</h1>
-				<StoryTitle />
-				<StoryBox />
-				<StoryInput />
-			</div>
-		)
-	}
+
+ //  handleSubmit: function(e) {
+	// // don't use html submit
+	// e.preventDefault();
+ //  },
+
+  render: function() {
+    return (
+	  <div className="fullGameView">
+        <h1>Fwibble!</h1>
+	    <StoryTitle />
+	    <StoryBox />
+	    <StoryInput />
+	  </div>
+    )
+  }
 });
 
 				// 
@@ -23,35 +29,4 @@ module.exports = React.createClass({
 // ReactDOM.render(<GameView />, document.getElementById('app'));
 
 
-//OLD WAY OF DOING IT:
-
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var StoryTitle = require('./StoryTitle.js');
-// var StoryBox = require('./StoryBox.js');
-// var StoryInput = require('./StoryInput.js');
-
-// var GameView = React.createClass({
-// 	getInitialState: function () {
-// 		return {
-
-// 		};
-// 	},
-// 	render: function () {
-// 		return (
-// 			<div className="GameView">
-// 				<h1>Fwibble!</h1>
-// 				<StoryTitle />
-// 				<StoryBox />
-// 				<StoryInput />
-// 			</div>
-// 		)
-// 	}
-// });
-
-
-// ReactDOM.render(<GameView />,
-// 	document.getElementById('app')
-// );
-
-// module.exports = GameView;
+// <form onSubmit=/* write function: handleNewInput */ >
--- app/components/gameview/StoryInput.js
+++ app/components/gameview/StoryInput.js
@@ -7,11 +7,13 @@ module.exports = React.createClass({
 		e.preventDefault();
 	},
 	render: function() {
-		return (
-			<form onSubmit={this.handleSubmit}> <br/> <br/>
-				<input type="text" placeholder="Enter 6 words" />
-				<button type="submit">Write</button>
-			</form>
+	  return (
+		<div class-name="storyInputDisplay">
+		  <form onSubmit={this.handleSubmit}>
+		    <input className="newStoryInput" type="text" placeholder="Enter 6 words"></input>
+		    <button type="submit">Submit</button>
+		  </form>
+	    </div>
 		)
 	}
 });
