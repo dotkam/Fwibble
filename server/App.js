980b22f310cd6408b4c0a3a72e316b56869c30d5
Natalie <natalie.s.sharpe@gmail.com>
2016-03-07 19:53:30 -0600

    rewriting paths for modules turned components

--- server/App.js
+++ server/App.js
@@ -1,16 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var RRouter = require('react-router');
-var Index = require('../modules/Index');
-var Signin = require('../modules/Signin');
-var Game = require('../modules/Game');
+var Index = require('../app/components/Index');
+var Signin = require('../app/components/Signin');
 var Gameview = require('../app/components/GameView')
 
 ReactDOM.render((
         <RRouter.Router history={RRouter.browserHistory} >
           <RRouter.Route path="/" component={Index}/>
           <RRouter.Route path="/signin" component={Signin}/>
-          <RRouter.Route path="/game" component={Game}/>
           <RRouter.Route path="/gameview" component={Gameview}/>
         </RRouter.Router>
     ), document.getElementById('app')
