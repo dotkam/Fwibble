8f74aff7c51908c1550cb01c6b65064894f4825f
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-03-09 16:21:30 -0600

    Completed model test functionality

--- test/app/actions/rooms_test.js
+++ test/app/actions/rooms_test.js
@@ -1,89 +1,136 @@
+"use strict"
 require('../../test-helper.js') // <--- This must be at the top of every test file.
 
-var Room = require(__app + '/actions/rooms');
-var User = require(__app + '/actions/users');
-var pg      = require('../../../db/db_setup');
-var dbCleaner = require('knex-cleaner');
+const Room = require(__app + '/actions/rooms');
+const pg      = require('../../../db/db_setup');
+const dbCleaner = require('knex-cleaner');
 
-describe('Room model', function() {
-	describe('can interface with a clean test database', function() {
+describe('Rooms model', function() {
+  describe('can interface with a clean test database', function() {
 
-  	beforeEach(function() {
+    beforeEach(function() {
       return dbCleaner.clean(pg, {mode: 'truncate'})
+        .catch(function(error) {
+          console.error('error inserting text', error)
+        })
 		.then(function() {
-    //       return pg('rooms').insert([
-    //         {
-    //           room_id:1,
-    //           room_hash: 'abc123',
-    //         }
-    //       ])
-    //     })
-    //     .then(function() {
-          return pg('users').insert([
+          return pg('rooms').insert([
             {
-              user_id:1,
-              username: 'Player1',
-              active_room: 1,
+              room_id: 22,
+              room_hash: 'abc123',
+              room_title: 'test room'
+            },
+            {
+              room_id: 27,
+              room_hash: 'ghi789',
+              room_title: 'yes, i am dog'
             }
           ])
         })
-    //     .then(function(){
-    //       return pg('texts').insert([
-    //         {
-    //           text_id: 1,
-    //           text_content: 'This is an example of Fwibble',
-    //           room_id: 1, 
-    //           user_id: 1,
-    //         },
-    //       ])
-    //     })
+        .then(function() {
+          return pg('users').insert([
+            {
+              user_id: 1,
+              username: 'Player1',
+              active_room: 22,
+            },
+            {
+              user_id: 2,
+              username: 'Player2',
+              active_room: 22,
+            },
+            {
+              user_id: 3,
+              username: 'Player3',
+              active_room: 22,
+            }
+          ])
+        })
+        .then(function() {
+          return pg('user_room').insert([
+            {
+              user_id: 1,
+              room_id: 22,
+            },
+            {
+              user_id: 2,
+              room_id: 22,
+            },
+            {
+              user_id: 3,
+              room_id: 22,
+            }
+          ])
+        })
+        .then(function(){
+          return pg('texts').insert([
+            {
+              text_id: 1,
+              text_content: 'This is an example of Fwibble',
+              room_id: 22, 
+              user_id: 1,
+            },
+          ])
+        })
+	})
+
+
+	it_('should create a first room', function * () {
+
+      let newRoom1 = {
+		room_hash: 'def456',
+		room_title: 'this is also room'
+      }
+      
+      yield Room.create(newRoom1)
+        .catch(function(error) {
+		  console.error('error inserting room', error);
+		})
+        .then(function(room) {
+          expect(room[0].room_hash).to.equal('def456');
+		  expect(room[0].room_title).to.equal('this is also room');
 		})
 	})
 
-	xit('should create a first room', function () {
+    it_('should find all users in a game room', function * () {
 
-		var newRoom1 = {
-			room_hash: 'abc123'
-		}
+      yield Room.allUser(22)
+        .catch(function(error) {
+		  console.error('error retrieving users', error);
+		})
+		.then(function(users) {
+		  expect(users).to.have.length(3);
+		  expect(users[0].user_id).to.equal(1);
+		  expect(users[1].user_id).to.equal(2);
+		  expect(users[2].user_id).to.equal(3);
+		})
+    })
 
-		Room.create(newRoom1)
-			.then(function(room) {
-				expect(room.room_id).to.equal(1);
-				expect(Room.allById()).to.have.length(1);
-			})
-			// .catch(function(err) {
-			// 	console.error('error inserting first room', err)
-			// })
-	})
+    it_('should find all active game rooms', function * () {
 
-	xit('should create a second room', function () {
-		var newRoom2 = {
-			room_hash: 'def456'
-		}
+      yield Room.allById()
+        .catch(function(error) {
+		  console.error('error retrieving rooms', error);
+		})
+		.then(function(rooms) {
+		  expect(rooms).to.have.length(2);
+		  expect(rooms[0].room_hash).to.equal('abc123');
+		  expect(rooms[1].room_hash).to.equal('ghi789');
+		  expect(rooms[0].room_title).to.equal('test room');
+		  expect(rooms[1].room_title).to.equal('yes, i am dog');
+		})
+    })
 
-		Room.create(newRoom2)
-			.then(function(room) {
-				expect(room.room_id).to.equal(2);
-				expect(Room.allById()).to.have.length(2);
-			})
-			// .catch(function(err) {
-			// 	console.error('error inserting second room', err)
-			// })
-	})
+    it_('should find game rooms by hash value', function * () {
 
-	xit('should add user1 and user2 to first room', function() {
-		User.create({username: 'user1', password: '', active_room: 1})
-		User.create({username: 'user2', password: '', active_room: 1})
-			.then(function() {
-				expect(User.findActiveRoom(1)).to.equal(1);
-				expect(User.findActiveRoom(2)).to.equal(1);
-			})
-	})
-
-	xit('should change user1 active_room and find all games for user1', function() {
-		// update user1 active_room to room_id: 2
-		// Room.allUser(1) should return all game rooms user1 has participated in
-		// ie room_id 1 & 2
-	})
-
+      yield Room.findIdByHash('ghi789')
+        .catch(function(error) {
+		  console.error('error retrieving rooms', error);
+		})
+		.then(function(rooms) {
+		  expect(rooms).to.have.length(1);
+		  expect(rooms[0].room_id).to.equal(27);
+		})
+    })
+})
 })
