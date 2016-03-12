3ec628be97a2a386642f8c534fbda6133b351b9b
Natalie <natalie.s.sharpe@gmail.com>
2016-03-11 19:50:19 -0600

    removed bundle.js from index.html - views and progress with bootstrap styling inside component

--- server/App.js
+++ server/App.js
@@ -18,16 +18,18 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-      <div class="container-fluid">
-      <div className='navbar'>
-      <h1>Fwibble</h1>
-      <ul className='nav-links'>
-        <li><Link to='/signin' className='fa fa-user'>Sign In</Link></li>
-        <li><Link to='/gameview' className='fa fa-pencil'>Game</Link></li>
-      </ul>
-    </div>
-  </div>
-        {this.props.children}
+        <div className="container-fluid">
+          <div className='navbar'>
+            <h1>Fwibble</h1>
+            <ul className='nav-links'>
+              <li><Link to='/signin' className='fa fa-user'>Sign In</Link></li>
+              <li><Link to='/gameview' className='fa fa-pencil'>Game</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className="container">
+          {this.props.children}
+        </div>
       </div>
     )
   }
