927522c01e88f4c164eed3c135a8797bcba4e470
Kamran Varahramyan <kamran@Kamrans-MacBook.local>
2016-03-15 12:44:52 -0500

    false signin status message

--- app/components/signin/Signin.js
+++ app/components/signin/Signin.js
@@ -13,7 +13,9 @@ module.exports = React.createClass({
   getInitialState: function() {
     return {
       username: '',
-      password: ''
+      password: '',
+      loginErr: false,
+      loginMsg: null
     }
   },
 
@@ -42,24 +44,44 @@ module.exports = React.createClass({
       contentType: 'application/json',
       success: function(data) {
         // data === whatever we respond with in user-api.js
-        console.log("success data:", data)
+        // console.log("success data:", data)
         // this.transitionTo('gameview') 
- 
-        this.props.setUser(data)
+        //Object {userStatus: false, passStatus: false, activeUser: null, 
+        // activeGame: null, errMessage: "Username not found. Please create an
+        
+        console.log('Login response object:', data)
+        // handle err message
+        if (data.errMessage!==null) {
+          this.setState({loginMsg: data.errMessage})
+          if (data.userStatus===false) {
+            this.setState({username: ""})
+            this.setState({password: ""})
+          } else {
+              this.setState({password: ""})
+          }
+          // trigger display of error message
+          this.setState({loginErr: true})
+        // or set active user
+        } else {
+          this.setState({loginErr: false})
+          this.props.setUser(data.activeUser)
+          // TODO: route to game page
+        }
+
+
         console.log('props:',this.props)
-        console.log(this.props.user)
+        // console.log(this.props.user)
 
       }.bind(this),
       error: function(data) {
-        console.error("error data:", data)
+        console.error("Connection error:", data)
       }.bind(this)
 
     });
-
-    this.setState({password: ""})
   },
 
   render: function() {
+    var loginMessage = this.state.loginErr ? <div>{this.state.loginMsg}</div> : null;
 
     return (
       <div>
@@ -72,7 +94,7 @@ module.exports = React.createClass({
 		        <br/>
 		        <input type="password" placeholder="password" value={this.state.password} onChange={this.handlePassword} />
 		        <br/>
-		        <input type="submit" name="signInSubmit" onClick={this.handleClick} />
+		        <input type="submit" name="signInSubmit" onClick={this.handleClick} />           {loginMessage}
 		      </form>
           <br />
           <div><Link to="/signup">Don't have an account yet? Sign up!</Link></div>
--- server/apis/user-api.js
+++ server/apis/user-api.js
@@ -31,25 +31,62 @@ UserAPI.post('/signup', function(req, res) {
 
 
 function signIn (req, res) {
+  var response = {
+    userStatus: false,
+    passStatus: false,
+    activeUser: null,
+    activeGame: null,
+    errMessage: null
+  };
   User.findIdByUsername(req.body.username)
+  // catch unknown username
+  .then(function(array) {
+    if (array.length>0) {
+      response.userStatus = true;
+      response.activeUser = req.body.username;
+    } else {
+      response.errMessage = 'Username not found. Please create an account.'
+    throw error
+    }
+  })
+  // ask model if password is good
+  .then(function(array) {
+    response.userStatus = true;
+    return User.checkPassword(array[0].user_id, req.body.password)
+  })
+  // if password is incorrect, respond and throw error
+  .then(function(bool) {
+    if (!bool) {
+      response.errMessage = 'Password incorrect. Please try again.'
+      throw error
+    } else {
+      response.passStatus = true;
+      response.activeUser = req.body.username;
+    }
+    console.log("Password response", array)
+  })
+  // otherwise, find active game
   .then(function(array) {
     if(array.length) {
       return User.findActiveGame(array[0].user_id)
-    } else {
-
     }
   })
+  // translate to gamehash
   .then(function(array) {
     console.log("GameHash", array)
     return Game.findIdByHash(array[0].active_game)
   })
+  // and send relevant response
   .then(function(array) {
     // res.send("active room: " + array[0].active_room)
     console.log("GameID: ",array[0].game_id)
+    response.activeGame = array[0].game_id;
     // res.send("GameID: " + array[0].game_id)
-    res.send(req.body.username)
+    res.send(response)
   })
+  // catch-all response
   .catch(function(err) {
-    res.send("could not login user: " + err)
+    console.error('catch all error:', response.errMessage)
+    res.send(response)
   })
 }
