aa435e07fed7a7edd823467d90476c36067c75ce
austinjkovach <kovach.a.j@gmail.com>
2016-03-21 14:21:17 -0500

    Reintegrating actions

--- server/App.js
+++ server/App.js
@@ -15,8 +15,12 @@ var Signup = require('../app/components/signup/Signup');
 var Signout = require('../app/components/signout/Signout');
 var Lobby = require('../app/components/lobby/Lobby');
 var Gameview = require('../app/components/gameview/GameView');
-//var userActions = //
+
 var Auth = require('./auth');
+var Users = require('../app/actions/users');
+var Games = require('../app/actions/games');
+var Sessions = require('../app/actions/sessions');
+var Fwibs = require('../app/actions/fwibs');
 
 var io = require('socket.io-client');
 var socket = io.connect();
@@ -32,8 +36,7 @@ var App = React.createClass({
   getInitialState: function() {
     return {username: null, loggedIn: Auth.loggedIn(), active_game: null} // Ask Gilbert if this belongs in the state
   },
-  loginUser: function(){
-  },
+  loginUser: Auth.login,
   logoutUser: function(){
     socket.emit('logout', {username: this.state.username});
     Auth.logout(); // log out on /signout
@@ -72,37 +75,13 @@ var App = React.createClass({
   setUser: function(data) {
     Auth.login();
     this.setState({
-
       username: data.username,
       loggedIn: Auth.loggedIn(),
       active_game: data.active_game
     });
-    // $.ajax({
-    //   type: 'POST',
-    //   url: '/user/game', //game
-    //   data: { username: username },
-    //   contentType: 'application/json',
-    //   success: function(data) {
-    //     // TODO update state based off of active_game response
-    //   },
-    //   error: function(data) {
-    //     console.error("Connection error:", data)
-    //   }
-    // });
-    
-    if(this.state.active_game){
-      this.context.router.replace(`/gameview/${this.state.active_game}`)
-    }
-    else {
-      this.context.router.replace(`/lobby`)
-    }
+    var next = this.state.active_game ? `/gameview/${this.state.active_game}` : `/lobby`;
+    this.context.router.replace(next)
   },
-  // setActiveState: function(data){
-  //   this.setState({
-  //     activeGame
-  //   })
-  // }
-
   render: function() {
 
   var navBarShow = this.state.loggedIn ? (<NavBar active_game={this.state.active_game} loggedIn={this.state.loggedIn} />) : null;
@@ -111,11 +90,15 @@ var App = React.createClass({
       <div>
         {navBarShow}
         {this.props.children && React.cloneElement(this.props.children, {
-          setUser: this.setUser,
           user: this.state.username,
+          active_game: this.state.active_game,
+          users: this.state.users,
+          fwibs: this.state.fwibs,
+          turn: this.state.turn,
+          loginUser: this.loginUser,
+          setUser: this.setUser,
           logoutUser: this.logoutUser,
-          joinGame: this.joinGame,
-          active_game: this.state.active_game
+          joinGame: this.joinGame
         })}
       </div>
     )
