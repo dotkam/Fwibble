282142792e4d92fd569c8e30c9732f0ee43be847
austinjkovach <kovach.a.j@gmail.com>
2016-03-23 16:27:59 -0500

    changed time back to 6 seconds - removed last word of titles

--- app/components/gameview/GameTimer.js
+++ app/components/gameview/GameTimer.js
@@ -7,7 +7,7 @@ var socket = io.connect();
 module.exports = React.createClass({
   
   getInitialState: function() {
-    return {secondsLeft: 30 };
+    return {secondsLeft: 6 };
   },
 
   componentDidMount: function() {
