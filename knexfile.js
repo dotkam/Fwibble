df3dcf9acb46ac75b606955978ca374b9fd083ec
austinjkovach <kovach.a.j@gmail.com>
2016-03-24 16:24:31 -0500

    added heroku postgres knex configs

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,9 @@
 module.exports = {
+
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://qcywsavdukyurg:11OCy451lnwuxf5CDE1RGbSWhY@ec2-54-204-6-113.compute-1.amazonaws.com:5432/d2jgvtmk0qg4vn'
+  },
   development: {
     client: 'postgresql',
     connection: {
---
+++ webpack-production.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './server/App.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  
+}
