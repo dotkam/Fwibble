a42c7920903c7dbe60fac14e8b83f7865461fb6e
Kamran Varahramyan <kamran@Kamrans-MacBook.local>
2016-03-24 12:40:41 -0500

    good meter

--- app/components/gameview/WordCountMeter.js
+++ app/components/gameview/WordCountMeter.js
@@ -2,13 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
-
-  render: function() {
-    console.log('wordCountMeter reading:', this.props.wordCount)
-
-    return(
-      <div className="progress">
-        <div className="progress-bar progress-bar-warning" style={{width: "15%"}}>
+        /* <div className="progress-bar progress-bar-warning" style={{width: "15%"}}>
           <span className="sr-only"> 5 words left (success)</span>
         </div>
         <div className="progress-bar progress-bar-warning" style={{width: "30%"}}>
@@ -28,7 +22,28 @@ module.exports = React.createClass({
         </div>
         <div className="progress-bar progress-bar-danger" style={{width: "100%"}}>
           <span className="sr-only">Too many words! (danger)</span>
-        </div>
+        </div> */
+
+  render: function() {
+    console.log('wordCountMeter reading:', this.props.wordCount)
+
+    var width;
+    if(this.props.wordCount>5) {width = '100%'}
+      else {
+        var value = "" + (100 * this.props.wordCount / 6);
+        width = value.slice(0,2) + '%';
+      };
+
+    console.log('width', width)
+
+    var styling;
+    if(this.props.wordCount<6) {styling = 'progress-bar progress-bar-warning'}
+      else if(this.props.wordCount===6) {styling = 'progress-bar progress-bar-success'}
+        else {styling = 'progress-bar progress-bar-danger'};
+
+    return(
+      <div className="progress">
+        <div className={styling} style={{width}}></div>
       </div>
     );
   }
