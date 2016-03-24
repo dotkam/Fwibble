75c35bd9648bea405e0484d00c09991ee064c488
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-03-24 12:23:53 -0500

    Completed user room styling, colors not final

--- app/components/gameview/UsersInRoom.js
+++ app/components/gameview/UsersInRoom.js
@@ -6,12 +6,12 @@ module.exports = React.createClass({
   render: function (){
     return (
       <div className='users panel panel-info'>
-        <h3 className='usersTitle'> Users in Room: </h3>
-        <ul>
+        <h3 className='usersTitle panel-heading'> Users in Room </h3>
+        <ul className='usersList panel-body'>
           {
             this.props.users.map((user, i) => {
               return (
-                <li key={i}>
+                <li className='list-unstyled' key={i}>
                   {user}
                 </li>
               );
