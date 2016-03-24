a9174eb4b78fba018f2acfc0a512a1895d3a8d52
austinjkovach <kovach.a.j@gmail.com>
2016-03-24 16:48:16 -0500

    Added webpack-prod file and package.json postinstall script

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,27 +1,37 @@
-var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/views/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 var webpack = require('webpack');
+
 
 module.exports = {
   devtool: 'source-map',
   entry: [
     './server/App.js'
   ],
+
   output: {
-    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/public/'
+    path: __dirname + '/dist'
   },
+  
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader', 'react-hot'] },
+      { test: /\.css$/, loader: "style-loader!css-loader"}
+    ]
+  },
+
   plugins: [
-    new webpack.optimize.DedupePlugin(),
+    HTMLWebpackPluginConfig, 
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       compress: {
         warnings: false
       }
     })
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  
-}
+  ]
+};
