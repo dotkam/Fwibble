6b7a621d752e6f793010ba21a445fa33c89ee1ec
austinjkovach <kovach.a.j@gmail.com>
2016-03-26 10:54:57 -0500

    File loader

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -23,7 +23,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader'}
+      { test: /\.css$/, loader: 'style-loader!css-loader'},
+      { test: /\.png$/, loader: 'file-loader'}
     ]
   },
 
