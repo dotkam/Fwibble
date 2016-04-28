0f5cb79d41bdaa2e94623d410d4603663f5bfa47
austinjkovach <kovach.a.j@gmail.com>
2016-04-28 13:12:53 -0500

    Changed socket calls titles for archive

--- app/components/archive/ArchiveGame.js
+++ app/components/archive/ArchiveGame.js
@@ -4,6 +4,8 @@ var ReactRouter = require('react-router');
 var io = require('socket.io-client');
 var socket = io.connect();
 
+// TODO Add Facebook/Twitter sharing
+
 module.exports = React.createClass({
   getInitialState: function(){
     return { fwibs: [] }
