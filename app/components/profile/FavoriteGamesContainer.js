c8bcf430b3a6567eafadb8a2b683bbc82db0a191
austinjkovach <kovach.a.j@gmail.com>
2016-05-01 18:00:02 -0500

    fixed button sizes and some button coloring

--- app/components/profile/FavoriteGamesContainer.js
+++ app/components/profile/FavoriteGamesContainer.js
@@ -17,13 +17,13 @@ module.exports = React.createClass({
                   return (
                     <div key={i}>
                       <li className="btn-group btn-group-justified">
-                        <div className="btn-group">
+                        <div className="btn-group btn-group-big">
                           <Link to={`archive/${game.game_hash}`} className="btn btn-primary btn-outline btn-lg">{game.game_title}</Link>
                         </div>
-                        <div className="btn-group">
-                          <button className="btn btn-favorite-true btn-lg" onClick={this.props.toggleFavorite.bind(null, {gamehash: game.game_hash, favorite: game.favorite})} ><div className="glyphicon glyphicon-star"></div></button>
+                        <div className="btn-group-small btn-group">
+                          <button className="btn btn-favorite btn-outline btn-lg" onClick={this.props.toggleFavorite.bind(null, {gamehash: game.game_hash, favorite: game.favorite})} ><div className="glyphicon glyphicon-star"></div></button>
                         </div>
-                        <div className="btn-group">
+                        <div className="btn-group btn-group-small">
                           <button className="btn btn-danger btn-outline btn-lg" onClick={this.props.deleteGame.bind(null, game.game_hash)} ><div className="glyphicon glyphicon-trash"></div></button>
                         </div>
                       </li>
