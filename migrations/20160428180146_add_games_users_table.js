ea68f01c0fe3cea5e421649b496a685885418059
austinjkovach <kovach.a.j@gmail.com>
2016-05-02 16:24:28 -0500

    Migrations 5

--- migrations/20160428180146_add_games_users_table.js
+++ migrations/20160428180146_add_games_users_table.js
@@ -6,12 +6,7 @@ exports.up = function(knex, Promise) {
     table.boolean('favorites').defaultTo(false);
   })
   .then(function(){
-    return knex.schema.createTableIfNotExists('games_users', function(table){
-        table.increments('id').primary();
-        table.integer('user_id').references('users.user_id');
-        table.string('game_hash').references('games.game_hash');
-        table.boolean('favorites').defaultTo(false);;
-    })
+    return;
   });
 };
 
