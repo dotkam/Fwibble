cbae7f108d387114748fecfdc924812f5743bd30
austinjkovach <kovach.a.j@gmail.com>
2016-05-02 16:21:22 -0500

    migrations 4

--- migrations/20160428174246_add_favorites_to_games_users.js
+++ migrations/20160428174246_add_favorites_to_games_users.js
@@ -1,16 +1,16 @@
 exports.up = function(knex, Promise) {
-  return  knex.schema.table('games_users', function(table){
-            table.boolean('favorites').defaultsTo(false);
-          })
-          .then(function(){
-            return  knex.schema.table('games_users', function(table){
-                      table.boolean('favorites').defaultsTo(false);
-                    })
-          });
+  // return  knex.schema.table('games_users', function(table){
+  //           table.boolean('favorites').defaultsTo(false);
+  //         })
+  //         .then(function(){
+  //           return  knex.schema.table('games_users', function(table){
+  //                     table.boolean('favorites').defaultsTo(false);
+  //                   })
+  //         });
 };
 
 exports.down = function(knex, Promise) {
-  return  knex.schema.table('games_users', function(table){
-            table.dropColumn('favorites');
-          })
+  // return  knex.schema.table('games_users', function(table){
+  //           table.dropColumn('favorites');
+  //         })
 };
